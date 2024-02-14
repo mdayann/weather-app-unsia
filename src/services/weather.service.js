175be872a054acaa13e0e2844a3f7b94f78da49f
mdayann <mdayan312@gmail.com>
2024-02-14 22:45:36 +0700

    feat: add new field for weather temp

--- src/services/weather.service.js
+++ src/services/weather.service.js
@@ -17,6 +17,27 @@ const queryWeather = async (lat, lon) => {
 
     const result = await response.json();
 
+    // Calculate temperature in Fahrenheit
+    const tempCelsius = result.main.temp;
+    const feelsLike = result.main.feels_like;
+    const tempMin = result.main.temp_min;
+    const tempMax = result.main.temp_max;
+
+    const tempFahrenheit = parseFloat(((tempCelsius * 9) / 5 + 32).toFixed(2));
+    const feelsLikeFahrenheit = parseFloat(((feelsLike * 9) / 5 + 32).toFixed(2));
+    const tempMinFahrenheit = parseFloat(((tempMin * 9) / 5 + 32).toFixed(2));
+    const tempMaxFahrenheit = parseFloat(((tempMax * 9) / 5 + 32).toFixed(2));
+
+    // Icon URL
+    const iconCode = result.weather[0].icon;
+
+    // Result storing
+    result.weather[0].icon = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    result.main.temp_fh = tempFahrenheit;
+    result.main.feels_like_fh = feelsLikeFahrenheit;
+    result.main.temp_min_fh = tempMinFahrenheit;
+    result.main.temp_max_fh = tempMaxFahrenheit;
+
     return result;
   } catch (error) {
     throw new ApiError(httpStatus.SERVICE_UNAVAILABLE, 'Error getting weather data');
